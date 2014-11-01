b5cbfc0ac52e47262a750875eea32542235f7e2c
Max <max3754@hotmail.com>
2014-11-01 20:10:27 +0100

    acquire and release for 1 on the server predefined lock

--- lock_server.cc
+++ lock_server.cc
@@ -7,8 +7,11 @@
 #include <arpa/inet.h>
 
 lock_server::lock_server():
-  nacquire (0)
+  nacquire (0),
+  lock(false)
 {
+	pthread_mutex_init(&lock_mutex, NULL);
+	pthread_cond_init(&lock_available, NULL);
 }
 
 lock_protocol::status
@@ -18,6 +21,30 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   printf("stat request from clt %d\n", clt);
   r = nacquire;
   return ret;
+}
+
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+	pthread_mutex_lock(&lock_mutex);
+	while (lock) {
+		pthread_cond_wait(&lock_available, &lock_mutex);
+	}
+	lock = true;
+	printf("clt %d locked\n", clt);
+	pthread_mutex_unlock(&lock_mutex);
+	return lock_protocol::OK;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+	pthread_mutex_lock(&lock_mutex);
+	lock = false;
+	printf("clt %d unlocked\n", clt);
+	pthread_cond_signal(&lock_available);
+	pthread_mutex_unlock(&lock_mutex);
+	return lock_protocol::OK;
 }
 
 
--- lock_server.h
+++ lock_server.h
@@ -5,6 +5,7 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
@@ -13,11 +14,16 @@ class lock_server {
 
  protected:
   int nacquire;
+  bool lock;
+  pthread_mutex_t lock_mutex;
+  pthread_cond_t lock_available;
 
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
